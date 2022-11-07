f079ff7d638b5854969f1f5f3c1c4a33b927c9dd
Karsei <karseidev@gmail.com>
2022-11-07 21:53:08 +0900

    intial commit

---
+++ src/main/java/kr/pe/karsei/mvcfeatures/MvcFeaturesApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.karsei.mvcfeatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcFeaturesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcFeaturesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/pe/karsei/mvcfeatures/MvcFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.karsei.mvcfeatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcFeaturesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
